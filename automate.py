288ad0fc2a6205319ab140d3be2657aa32213b51
Gabriel Ve Vance <42804193+gvevance@users.noreply.github.com>
2022-01-03 12:45:05 +0530

    Write to file code added

--- automate.py
+++ automate.py
@@ -1,4 +1,5 @@
 # run in custom environment with the required packages
+# TODO: Add an interactive mode for the circuit
 
 import numpy as np
 
@@ -12,8 +13,47 @@ r2  = 20e3
 c2  = 0.8e-12
 
 filename = "dummy.cir"
-with open(filename,"r") as file:
-    lines = file.readlines()
 
-for i in lines:
-    print(i,end="")
+contents = f'''2 stage OTA. Final aim - use RL agent to design it
+
+* Circuit definition
+vin 1 0 dc 0 ac 1
+gm1 2 0 1 0 -{gm1}
+r1 2 0 {r1}
+c1 2 0 {c1}
+cc 2 3 {cc}
+gm2 3 0 2 0 {gm2}
+r2 3 0 {r2}
+c2 3 0 {c2}
+
+* AC analysis
+.ac dec 10 1 10G
+
+* measuring phase margin
+.measure ac phase_margin 
++ FIND vp(3) WHEN vdb(3) = 0
+.measure ac phase_margin_deg param = ' 57.29*phase_margin '
+
+* measuring DC gain
+.measure ac DC_gain_dB
++ MAX vdb(3) from=1 to=10G 
+
+* measuring UGF
+.measure ac UGF
++ WHEN vdb(3)=0 CROSS=1
+
+.control
+run
+
+* Magnitude dB plot for v(3) on log scale
+* plot vdb(3) xlog
+* Phase in degrees plot for v(3) on log scale
+* plot {{57.29*vp(3)}} xlog
+
+.endc
+.end
+'''
+
+# print(string1)
+with open(filename,"w") as file:
+    file.write(contents)
